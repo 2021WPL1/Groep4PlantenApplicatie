231f1222db5c5f0043dd83a82e551e590c178283
Lily Vandenbore <lily.vandenbore@student.vives.be>
2021-06-02 13:56:29 +0200

    Remove unused imports

--- PlantenApplicatie/viewmodels/TabsViewModels/TabFenoTypeViewModel.cs
+++ PlantenApplicatie/viewmodels/TabsViewModels/TabFenoTypeViewModel.cs
@@ -2,10 +2,8 @@
 using PlantenApplicatie.Domain;
 using Prism.Commands;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Input;
 
