3e0eb054b9aee7617e761ab8b0d12b6defbe69ee
Lily Vandenbore <lily.vandenbore@student.vives.be>
2021-06-03 14:05:09 +0200

    Add functionality to dao to add last login date / time

--- PlantenApplicatie/UserTab/ViewModels/AddGebruikerViewModel.cs
+++ PlantenApplicatie/UserTab/ViewModels/AddGebruikerViewModel.cs
@@ -15,29 +15,29 @@ namespace PlantenApplicatie.viewmodels
         private readonly PlantenDao _dao;
 
         //private variables for the GUI
-        private string _SelectedRole;
-        private string? _TextInputFirstName;
-        private string? _TextInputLastName;
-        private string? _TextInputEmail;
+        private string _selectedRole;
+        private string? _textInputNumber;
+        private string? _textInputFirstName;
+        private string? _textInputLastName;
+        private string? _textInputEmail;
         private string _passwordErrorMessage;
         //observable collection for the combobox
 
-
-        private Gebruiker _user;
-        public ObservableCollection<string> Roles { get; set; }
+        private readonly Gebruiker _user;
+        public ObservableCollection<string> Roles { get; }
 
         //buttoncommand to save an user in the database
-        public ICommand AddUserCommand { get; set; }
+        public ICommand AddUserCommand { get; }
 
         //variables Davy
-        public ICommand CloseWindowCommand { get; set; }
-        private Window _addGebruikerWindow;
+        public ICommand CloseWindowCommand { get; }
+        private readonly Window _addGebruikerWindow;
 
         private Brush _color;
 
         public AddGebruikerViewModel(Window window, Gebruiker user)
         {
-            _addGebruikerWindow = window;       // Davy   
+            _addGebruikerWindow = window; // Davy   
             _user = user; // Davy
             _dao = PlantenDao.Instance;
             Roles = new ObservableCollection<string>();
@@ -58,43 +58,53 @@ namespace PlantenApplicatie.viewmodels
             }
         }
 
+        public string? TextInputNumber
+        {
+            get => _textInputNumber;
+            set
+            {
+                _textInputNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
         //getters setters (Jim)
         public string? TextInputFirstName
         {
-            get => _TextInputFirstName;
+            get => _textInputFirstName;
             set
             {
-                _TextInputFirstName = value;
+                _textInputFirstName = value;
                 OnPropertyChanged();
             }
         }
         
         public string? TextInputLastName
         {
-            get => _TextInputLastName;
+            get => _textInputLastName;
             set
             {
-                _TextInputLastName = value;
+                _textInputLastName = value;
                 OnPropertyChanged();
             }
         }
 
         public string? TextInputEmail
         {
-            get => _TextInputEmail;
+            get => _textInputEmail;
             set
             {
-                _TextInputEmail = value;
+                _textInputEmail = value;
                 OnPropertyChanged();
             }
         }
 
         public string SelectedRole
         {
-            get => _SelectedRole;
+            get => _selectedRole;
             set
             {
-                _SelectedRole = value;
+                _selectedRole = value;
                 OnPropertyChanged();
             }
         }
@@ -108,19 +118,19 @@ namespace PlantenApplicatie.viewmodels
                 OnPropertyChanged();
             }
         }
-        //load in the roles, for now database is empty so the roles are hardcoded to access the program (Jim)
-        public void LoadRoles()
-        {
-            
-                Roles.Add("manager");
-                Roles.Add("data-collector");
-                Roles.Add("gebruiker");
-           
-        }
+        
         //check the passwords on if they are equal
         public void PasswordChecker(string password, string passwordConfirm)
         {
             PasswordErrorMessage = password == passwordConfirm ? string.Empty : "Paswoorden zijn niet gelijk";
+        }
+        
+        //load in the roles, for now database is empty so the roles are hardcoded to access the program (Jim)
+        private void LoadRoles()
+        {
+            Roles.Add("manager");
+            Roles.Add("data-collector");
+            Roles.Add("gebruiker");
         }
 
         private void AddUser(PasswordBox passwordBox)
@@ -157,7 +167,7 @@ namespace PlantenApplicatie.viewmodels
             MessageBox.Show(message);
         }
 
-        private bool IsUserValid(string number, string firstName, string lastName, string email)
+        private static bool IsUserValid(string number, string firstName, string lastName, string email)
         {
             email = email.Split("@")[0].ToLower();
             
@@ -167,7 +177,7 @@ namespace PlantenApplicatie.viewmodels
         private void CloseWindow()
         {
             // herladen Users door nieuw venster BeheerPlanten op te starten
-            BeheerPlanten managePlants = new BeheerPlanten(_user);
+            BeheerPlanten managePlants = new(_user);
             managePlants.Show();
 
             _addGebruikerWindow.Close();
